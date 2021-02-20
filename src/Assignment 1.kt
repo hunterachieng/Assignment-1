e657bf8e073915f94c690920f1d7e3b6b664bbdf
hunterachieng <achienghunter@gmail.com>
2021-02-20 22:31:47 +0300

    first commit

---
+++ src/Assignment 1.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var fullname="Synthia Hunter Achieng"
+    var age=23
+    var phonenumber="0721567839"
+    var weightinKg=53
+    var boolKenyanCitizen=true
+if(boolKenyanCitizen){
+    println("True");
+}else{if(boolKenyanCitizen==false);
+    println("False");}
+    }
